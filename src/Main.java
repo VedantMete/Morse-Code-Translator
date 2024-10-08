cc938dc81e8765727ef869570c648a1b78eb2ebb
VedantMete <vedantmete@gmail.com>
2024-10-08 15:12:16 +0530

    intial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i=0;i<n;i++){
+           arr[i] = sc.nextInt();
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+
     }
 }
